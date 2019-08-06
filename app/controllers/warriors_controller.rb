710bf1046bb854342c1aec42b2c87e71862515a1
Myriam Chahlafi <myriam.chah@gmail.com>
2019-08-06 12:26:58 +0200

    Generate Warriors controller + crud

---
+++ app/controllers/warriors_controller.rb
@@ -0,0 +1,48 @@
+class WarriorsController < ApplicationController
+  before_action :set_warrior, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @warriors = Warrior.all
+  end
+
+  def show
+  end
+
+  def new
+    @warrior = Warrior.new
+  end
+
+  def create
+    @warrior = Warrior.new(warrior_params)
+    if @warrior.valid?
+      @warrior.save
+      redirect_to warriors_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @warrior.update(warrior_params)
+    redirect_to warriors_path
+  end
+
+  def destroy
+    @warrior.destroy
+    redirect_to warriors_path
+  end
+
+  private
+
+  def warrior_params
+    params.require(:warrior).permit(:name, :intro, :avatar, :life_points, :strength, :xp)
+  end
+
+  def set_warrior
+    @warrior = Warrior.find(params[:id])
+  end
+end
+
