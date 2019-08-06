43f2723aa43c23642e3406646a2247a7f3afcd84
Myriam Chahlafi <myriam.chah@gmail.com>
2019-08-06 12:36:42 +0200

    Write basic tests on warrior model

--- test/models/warrior_test.rb
+++ test/models/warrior_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class WarriorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have a name" do
+    warrior = Warrior.new(life_points: 200, strength: 10)
+    assert_not warrior.valid?
+  end
+
+  test "life points should be less than 200" do
+    warrior = Warrior.new(name: "Bobo", life_points: 300, strength: 10)
+    assert_not warrior.valid?
+  end
+
+  test "strength should be less than 10" do
+    warrior = Warrior.new(name: "Bobo", life_points: 300, strength: 10)
+    assert_not warrior.valid?
+  end
+
+  test "xp default value should be 0" do
+    warrior = Warrior.new
+    assert_equal 0, warrior.xp
+  end
 end
