dfea59e75b2f00a1a45f82b414922d0002711730
Myriam Chahlafi <myriam.chah@gmail.com>
2019-08-07 16:59:45 +0200

    Update fights controller to get strokes and winner

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -1,5 +1,6 @@
 class FightsController < ApplicationController
   before_action :set_fight, only: [:show]
+  before_action :set_fighters, only: [:create]
 
   def index
     @fights = Fight.all
@@ -14,21 +15,28 @@ class FightsController < ApplicationController
 
   def create
     @fight = Fight.new(fight_params)
+    @fight.strokes = helpers.strokes
+    @fight.winner = helpers.winner
     if @fight.valid?
       @fight.save
       redirect_to fights_path
     else
-      render :show
+      render :new
     end
   end
 
   private
 
   def fight_params
-    params.require(:fight).permit(:fighter_1, :fighter_2)
+    params.require(:fight).permit(:fighter_1_id, :fighter_2_id)
   end
 
   def set_fight
     @fight = Fight.find(params[:id])
+  end
+
+  def set_fighters
+    @fighter_1 = Warrior.find(params["fight"]["fighter_1_id"].to_i)
+    @fighter_2 = Warrior.find(params["fight"]["fighter_2_id"].to_i)
   end
 end
--- app/helpers/fights_helper.rb
+++ app/helpers/fights_helper.rb
@@ -1,2 +1,19 @@
 module FightsHelper
+    def strokes
+    stroke_2 = @fighter_1.life_points / @fighter_2.strength
+    stroke_1 = @fighter_2.life_points / @fighter_1.strength
+    return stroke_1 + stroke_2
+  end
+
+  def winner
+    stroke_2 = @fighter_1.life_points / @fighter_2.strength.to_f
+    stroke_1 = @fighter_2.life_points / @fighter_1.strength.to_f
+    if stroke_1 == stroke_2
+      return "Ex-aequo"
+    elsif stroke_1 < stroke_2
+      return @fighter_1.name
+    else
+      return @fighter_2.name
+    end
+  end
 end
