2bc5e55521c444b6740473255df5d774541cb5b7
Myriam Chahlafi <myriam.chah@gmail.com>
2019-08-07 15:18:07 +0200

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+batcat = Warrior.create(name: "Batcat", intro: "A billionaire cat with bat wings...", life_points: 200, strength: 7)
+batcat.avatar.attach(io: File.open('app/assets/images/batcat.jpg'), filename: 'batcat.jpg')
+
+supercat = Warrior.create(name: "Supercat", intro: "Some say his strength comes from an other galaxy, who will find his weakness?", life_points: 180, strength: 8)
+supercat.avatar.attach(io: File.open('app/assets/images/supercat.jpg'), filename: 'supercat.jpg')
+
+ladycat = Warrior.create(name: "Ladycat", intro: "Don't trust her polished claws!", life_points: 170, strength: 10)
+ladycat.avatar.attach(io: File.open('app/assets/images/pinkcat.jpg'), filename: 'pinkcat.jpg')
