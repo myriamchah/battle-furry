484388220f6de059cf0d4e2cee8210c22aa92ada
Myriam Chahlafi <myriam.chah@gmail.com>
2019-08-09 11:27:46 +0200

    Add statistics on warriors show

--- app/helpers/warriors_helper.rb
+++ app/helpers/warriors_helper.rb
@@ -7,16 +7,30 @@ module WarriorsHelper
     end
   end
 
-#return the name and number of victories of the best catfighter
+#return the name and number of victories of the best catfighter, used on fights#index
   def best_winner
     wins = Fight.all.group_by(&:winner).transform_values(&:count)
     return "The best catfighter is #{wins.max_by(&:last).first}, with #{wins.max_by(&:last).last} victories!"
   end
 
-#return the name and number of points of the most experienced fighter
+#return the name and number of points of the most experienced fighter, used on fights#index
   def best_xp
     warrior = Warrior.all.max_by(&:xp)
     return "The most experienced fighter is #{warrior.name}, with #{warrior.xp} XP points."
   end
 
+#return nb of fights
+  def fights_nb
+    @warrior.fights_as_fighter_1.count + @warrior.fights_as_fighter_2.count
+  end
+
+#return nb of victories
+  def wins_nb
+    Fight.all.where(winner: @warrior.name).count
+  end
+
+#return % of victories
+  def wins_rate
+    wins_nb / fights_nb * 100
+  end
 end
